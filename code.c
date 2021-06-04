1adb9256d6e30b80f21e7bc379f575faca714304
youssif khaled shawky <85262382+youssif1807198@users.noreply.github.com>
2021-06-04 22:20:52 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,15 @@
+void GreenLED_Status(uint8_t distance){
+SYSCTL_RCGCGPIO_R |=0x20;
+while ((SYSCTL_PRGPIO_R&0x20)==0);
+GPIO_PORTF_LOCK_R  =	GPIO_LOCK_KEY;
+GPIO_PORTF_CR_R |=0x08;
+GPIO_PORTF_AMSEL_R &= ~(0x08);
+GPIO_PORTF_AFSEL_R &= ~(0x08);
+GPIO_PORTF_PCTL_R &= 0xFFFF0FFF;
+GPIO_PORTF_DEN_R |= 0x08;
+GPIO_PORTF_DIR_R |= 0x08;
+	if(distance>=100){
+GPIO_PORTF_DATA_R |= 0x08;	
+	}
 
+}
