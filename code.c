80fc5ab30f1805f90edb57161f4572c448935507
youssif khaled shawky <85262382+youssif1807198@users.noreply.github.com>
2021-06-04 22:26:53 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,4 +1,4 @@
-void GreenLED_Status(uint8_t distance){
+void GreenLED_Status(uint8_t distance){ // this function turns green led on when we reached 100 or more meters
 SYSCTL_RCGCGPIO_R |=0x20;
 while ((SYSCTL_PRGPIO_R&0x20)==0);
 GPIO_PORTF_LOCK_R  =	GPIO_LOCK_KEY;
