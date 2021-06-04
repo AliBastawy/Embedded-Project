87bc13d30ee46a9f6206340dbb5120975706334e
AliBastawy <77601621+AliBastawy@users.noreply.github.com>
2021-06-04 20:23:15 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,45 @@
+void LCD_Init(void){
+	
+	/*
+	here we want to initiallize all the pins in Port B  and PA5 PA6 PA7 In port A we use port B for the data and command for LCD
+	and PA5 is the Rs(register selector) , PA6 is RW and PA7 E. we make all these Pins GPIO , Digtal , Output and enable change to
+  them   and finnaly set some command to initialize the LCD like clear,8-bit data,...	
+	
+	*/
+	SYSCTL_RCGCGPIO_R |= 0x03;
+	while(  (SYSCTL_PRGPIO_R&0x03) ==0  ) ;
+	
 
+	GPIO_PORTA_CR_R |= 0xE0;
+	GPIO_PORTB_CR_R |= 0xFF;
+	
+	GPIO_PORTA_AMSEL_R &=(unsigned long) ~(0xE0);
+	GPIO_PORTB_AMSEL_R &=(unsigned long) ~(0xFF);
+
+	
+	GPIO_PORTA_AFSEL_R &=(unsigned long) ~(0xE0);
+	GPIO_PORTA_PCTL_R  &= 0x000FFFFFF;
+	GPIO_PORTB_AFSEL_R &= (0x00);
+	GPIO_PORTB_PCTL_R  &= 0x00000000;
+	
+	GPIO_PORTA_DEN_R |= 0xE0;
+	GPIO_PORTB_DEN_R |= 0xFF;
+	
+	GPIO_PORTA_DIR_R |= 0xE0;
+	GPIO_PORTB_DIR_R |= 0xFF;
+	
+	GPIO_PORTA_DATA_R &=(unsigned long) ~(0xE0);
+	GPIO_PORTB_DATA_R &= (0x00);
+	
+	LCD_Command(0x38);   //8-bit data command
+	LCD_Command(0x06);   //shift crusor right command
+	LCD_Command(0x0F);   //turn display on command
+	LCD_Command(0x01);   //clear screen command
+}
+
+int main()
+{
+  
+  LCD_Init();
+  
+}
