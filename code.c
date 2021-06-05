fc334dbabd191aa88a3b6a746876191840f9e941
MarwanELMasry <85262373+MarwanELMasry@users.noreply.github.com>
2021-06-05 19:36:21 +0200

    Update code.c

--- code.c
+++ code.c
@@ -80,7 +80,32 @@
 #define PF2                     (*((volatile unsigned long *)0x40025010))
 #define PF1                     (*((volatile unsigned long *)0x40025008))
 #define PF0                     (*((volatile unsigned long *)0x40025004))
-	
+
+
+//*****************************************************************************
+//
+// UART registers (UART0)
+//
+//*****************************************************************************
+#define UART0_DR_R              (*((volatile unsigned long *)0x4000C000))
+#define UART0_RSR_R             (*((volatile unsigned long *)0x4000C004))
+#define UART0_ECR_R             (*((volatile unsigned long *)0x4000C004))
+#define UART0_FR_R              (*((volatile unsigned long *)0x4000C018))
+#define UART0_ILPR_R            (*((volatile unsigned long *)0x4000C020))
+#define UART0_IBRD_R            (*((volatile unsigned long *)0x4000C024))
+#define UART0_FBRD_R            (*((volatile unsigned long *)0x4000C028))
+#define UART0_LCRH_R            (*((volatile unsigned long *)0x4000C02C))
+#define UART0_CTL_R             (*((volatile unsigned long *)0x4000C030))
+#define UART0_IFLS_R            (*((volatile unsigned long *)0x4000C034))
+#define UART0_IM_R              (*((volatile unsigned long *)0x4000C038))
+#define UART0_RIS_R             (*((volatile unsigned long *)0x4000C03C))
+#define UART0_MIS_R             (*((volatile unsigned long *)0x4000C040))
+#define UART0_ICR_R             (*((volatile unsigned long *)0x4000C044))
+#define UART0_DMACTL_R          (*((volatile unsigned long *)0x4000C048))
+#define UART0_9BITADDR_R        (*((volatile unsigned long *)0x4000C0A4))
+#define UART0_9BITAMASK_R       (*((volatile unsigned long *)0x4000C0A8))
+#define UART0_PP_R              (*((volatile unsigned long *)0x4000CFC0))
+#define UART0_CC_R              (*((volatile unsigned long *)0x4000CFC8))	
 ////////////////////////////////////////////////////////////////////////////////////
 
 
