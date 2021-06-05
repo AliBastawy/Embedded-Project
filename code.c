2d582a40bbbf2b9de7ca8f93915ea4072b763845
alyashraf99 <85341443+alyashraf99@users.noreply.github.com>
2021-06-05 12:04:03 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,1 +1,15 @@
+// to calculate the distance 
 
+
+void distance(){
+
+Delayms(delay2);                //function to make delay(delay2 = 2000 ms defined in the header file) between  the readings of the GPS
+
+ReadData();	                // to set the new latitude and longitude from the GPS
+
+Totaldistance += haversine();	// haversine is a function to calculate distance between 2 locations
+
+previouslat = latitude; 		
+previouslong = longitude;
+
+}
