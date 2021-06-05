a0fe1925efae20493e255a4197e5485766298bd8
MarwanELMasry <85262373+MarwanELMasry@users.noreply.github.com>
2021-06-05 19:34:32 +0200

    Update code.c

--- code.c
+++ code.c
@@ -1,4 +1,89 @@
-#include "tm4c123gh6pm.h"
+//*********
+//
+// GPIO registers (PORTA)
+//
+//*********
+
+#define GPIO_PORTA_DATA_BITS_R  ((volatile unsigned long *)0x40004000)
+#define GPIO_PORTA_DATA_R       (*((volatile unsigned long *)0x400043FC))
+#define GPIO_PORTA_DIR_R        (*((volatile unsigned long *)0x40004400))
+#define GPIO_PORTA_IS_R         (*((volatile unsigned long *)0x40004404))
+#define GPIO_PORTA_IBE_R        (*((volatile unsigned long *)0x40004408))
+#define GPIO_PORTA_IEV_R        (*((volatile unsigned long *)0x4000440C))
+#define GPIO_PORTA_IM_R         (*((volatile unsigned long *)0x40004410))
+#define GPIO_PORTA_RIS_R        (*((volatile unsigned long *)0x40004414))
+#define GPIO_PORTA_MIS_R        (*((volatile unsigned long *)0x40004418))
+#define GPIO_PORTA_ICR_R        (*((volatile unsigned long *)0x4000441C))
+#define GPIO_PORTA_AFSEL_R      (*((volatile unsigned long *)0x40004420))
+#define GPIO_PORTA_DR2R_R       (*((volatile unsigned long *)0x40004500))
+#define GPIO_PORTA_DR4R_R       (*((volatile unsigned long *)0x40004504))
+#define GPIO_PORTA_DR8R_R       (*((volatile unsigned long *)0x40004508))
+#define GPIO_PORTA_ODR_R        (*((volatile unsigned long *)0x4000450C))
+#define GPIO_PORTA_PUR_R        (*((volatile unsigned long *)0x40004510))
+#define GPIO_PORTA_PDR_R        (*((volatile unsigned long *)0x40004514))
+#define GPIO_PORTA_SLR_R        (*((volatile unsigned long *)0x40004518))
+#define GPIO_PORTA_DEN_R        (*((volatile unsigned long *)0x4000451C))
+#define GPIO_PORTA_LOCK_R       (*((volatile unsigned long *)0x40004520))
+#define GPIO_PORTA_CR_R         (*((volatile unsigned long *)0x40004524))
+#define GPIO_PORTA_AMSEL_R      (*((volatile unsigned long *)0x40004528))
+#define GPIO_PORTA_PCTL_R       (*((volatile unsigned long *)0x4000452C))
+#define GPIO_PORTA_ADCCTL_R     (*((volatile unsigned long *)0x40004530))
+#define GPIO_PORTA_DMACTL_R     (*((volatile unsigned long *)0x40004534))
+
+//*********
+//
+// GPIO registers (PORTB)
+//
+//*********
+#define GPIO_PORTB_DATA_BITS_R  ((volatile unsigned long *)0x40005000)
+#define GPIO_PORTB_DATA_R       (*((volatile unsigned long *)0x400053FC))
+#define GPIO_PORTB_DIR_R        (*((volatile unsigned long *)0x40005400))
+#define GPIO_PORTB_IS_R         (*((volatile unsigned long *)0x40005404))
+#define GPIO_PORTB_IBE_R        (*((volatile unsigned long *)0x40005408))
+#define GPIO_PORTB_IEV_R        (*((volatile unsigned long *)0x4000540C))
+#define GPIO_PORTB_IM_R         (*((volatile unsigned long *)0x40005410))
+#define GPIO_PORTB_RIS_R        (*((volatile unsigned long *)0x40005414))
+#define GPIO_PORTB_MIS_R        (*((volatile unsigned long *)0x40005418))
+#define GPIO_PORTB_ICR_R        (*((volatile unsigned long *)0x4000541C))
+#define GPIO_PORTB_AFSEL_R      (*((volatile unsigned long *)0x40005420))
+#define GPIO_PORTB_DR2R_R       (*((volatile unsigned long *)0x40005500))
+#define GPIO_PORTB_DR4R_R       (*((volatile unsigned long *)0x40005504))
+#define GPIO_PORTB_DR8R_R       (*((volatile unsigned long *)0x40005508))
+#define GPIO_PORTB_ODR_R        (*((volatile unsigned long *)0x4000550C))
+#define GPIO_PORTB_PUR_R        (*((volatile unsigned long *)0x40005510))
+#define GPIO_PORTB_PDR_R        (*((volatile unsigned long *)0x40005514))
+#define GPIO_PORTB_SLR_R        (*((volatile unsigned long *)0x40005518))
+#define GPIO_PORTB_DEN_R        (*((volatile unsigned long *)0x4000551C))
+#define GPIO_PORTB_LOCK_R       (*((volatile unsigned long *)0x40005520))
+#define GPIO_PORTB_CR_R         (*((volatile unsigned long *)0x40005524))
+#define GPIO_PORTB_AMSEL_R      (*((volatile unsigned long *)0x40005528))
+#define GPIO_PORTB_PCTL_R       (*((volatile unsigned long *)0x4000552C))
+#define GPIO_PORTB_ADCCTL_R     (*((volatile unsigned long *)0x40005530))
+#define GPIO_PORTB_DMACTL_R     (*((volatile unsigned long *)0x40005534))
+//*********
+//
+// GPIO registers (PORTF)
+//
+//*********
+
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+#define PF4                     (*((volatile unsigned long *)0x40025040))
+#define PF3                     (*((volatile unsigned long *)0x40025020))
+#define PF2                     (*((volatile unsigned long *)0x40025010))
+#define PF1                     (*((volatile unsigned long *)0x40025008))
+#define PF0                     (*((volatile unsigned long *)0x40025004))
+	
+////////////////////////////////////////////////////////////////////////////////////
+
+
 #include "tm4c123.h"
 #include <stdio.h>
 #include <math.h>
