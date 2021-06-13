5fec8c3ef3f3fc59bcdf6619ad80dfc62028cd13
MarwanELMasry <85262373+MarwanELMasry@users.noreply.github.com>
2021-06-14 00:01:20 +0200

    Update GPSFunctions.h

--- GPSFunctions.h
+++ GPSFunctions.h
@@ -4,5 +4,27 @@
 #include "GlobalvariablesFunctionPrototype.h"
 
 
+////////////////////////////////////////////////////    GPS functions    ////////////////////////////////////////////////////// 
+void UART5_Init(void){
+    SYSCTL_RCGCUART_R |= 0x20;
+    while((SYSCTL_PRUART_R & 0x20) == 0);
+    SYSCTL_RCGCGPIO_R |= 0x10;
+		while((SYSCTL_PRGPIO_R & 0x10) == 0){}
+	
+	  GPIO_PORTE_CR_R |= 0x30;
+    GPIO_PORTE_AMSEL_R &= ~0x30;
+    GPIO_PORTE_AFSEL_R |= 0x30;
+    GPIO_PORTE_PCTL_R = (GPIO_PORTE_PCTL_R & ~0x00FF0000) | 0x00110000;
+    GPIO_PORTE_DEN_R |= 0x30;
+
+
+    UART5_CTL_R &= ~UART_CTL_UARTEN;
+    //set buad rate devider
+    UART5_IBRD_R = 104;
+    UART5_FBRD_R = 11;
+	  UART5_LCRH_R = (UART_LCRH_WLEN_8 | UART_LCRH_FEN);
+    UART5_CTL_R |= (UART_CTL_UARTEN | UART_CTL_RXE | UART_CTL_TXE);
+}
+
 
 #endif
