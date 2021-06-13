a214d43c3113dce2057f1d8209ff7d005f5d710c
AliBastawy <77601621+AliBastawy@users.noreply.github.com>
2021-06-13 23:47:08 +0200

    Update LedFunctions.h

--- LedFunctions.h
+++ LedFunctions.h
@@ -3,5 +3,23 @@
 #include "GlobalvariablesFunctionPrototype.h"
 
 
+///////////////////////////////////////////////////////   LED functions ////////////////////////////////////////////////////////
+
+void PortFF_Int(void){
+
+	SYSCTL_RCGCGPIO_R |=0x20;
+	while ((SYSCTL_PRGPIO_R&0x20)==0);
+	GPIO_PORTF_LOCK_R  =	GPIO_LOCK_KEY;
+	GPIO_PORTF_CR_R |=0x1F;
+	GPIO_PORTF_AMSEL_R &= ~(0x1F);
+	GPIO_PORTF_AFSEL_R &= ~(0x1F);
+	GPIO_PORTF_PCTL_R &= 0xFFF00000;
+	GPIO_PORTF_DEN_R |= 0x1F;
+	GPIO_PORTF_DIR_R |= 0x0E;
+	GPIO_PORTF_PUR_R |= 0x11;
+
+}
+
+
 
 #endif
